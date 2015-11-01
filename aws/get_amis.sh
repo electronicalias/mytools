daf2840dd0fb5a9248ee5bfef5f4f783bc6fa4ce
Philip Smith <electronicalias@gmail.com>
2015-11-01 01:12:19 +0000

    Changing so it only outputs the key (Region) and value (ImageId)

--- aws/get_amis.sh
+++ aws/get_amis.sh
@@ -12,10 +12,11 @@ function get_amis() {
     --region "$region_name" \
     --query "reverse(sort_by(Images[? !contains(Name, 'rc')], &CreationDate))
              [*].[ImageId,CreationDate]" \
-    --output text
+    --output text \
+    | awk '{ print $1 }' \
+    | head -1
 }
 
 for region in $(aws ec2 describe-regions --region eu-west-1 --query "sort(Regions[].RegionName)" --output text); do
-  echo  -e "Getting images for Region: \t ${region}"
-  get_amis "${region}"
+  echo  -e "${region} $(get_amis ${region})"
 done
