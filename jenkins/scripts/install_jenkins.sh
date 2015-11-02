a2abd27dd1a649d17ea6aded97ce750ee7720937
Philip Smith <electronicalias@gmail.com>
2015-11-02 11:24:11 +0000

    Adding Jenkins Scripts

---
+++ jenkins/scripts/install_jenkins.sh
@@ -0,0 +1,5 @@
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+yum install jenkins java -y
+service jenkins start
+chkconfig jenkins on
