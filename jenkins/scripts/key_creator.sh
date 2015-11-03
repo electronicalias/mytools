04b6be28292335d4e31c536f937f434d2eb3c872
electronicalias <electronicalias@gmail.com>
2015-11-03 14:21:18 +0000

    Added creation of a key that can be used to log into the host and configure it

--- jenkins/scripts/key_creator.sh
+++ jenkins/scripts/key_creator.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-aws ec2 create-key-pair --key-name "${1}" > key_json
+UseKey=$(aws ec2 describe-key-pairs --query "KeyPairs[?KeyName=='"${1}"'].KeyName" --output text)
+
+if [ "${1}" != "${UseKey}" ]; then
+  echo "Creating key for ${1}"
+  aws ec2 create-key-pair --key-name ${1} --query KeyMaterial --output text > ${1}.pem
+  echo "Created ${1} key"
+else
+  echo "Key already created for ${1}"
+fi
