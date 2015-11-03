b50f290b6a18a24db42a0ef5410e628eb3830b24
electronicalias <electronicalias@gmail.com>
2015-11-03 00:14:21 +0000

    Updated to remove peering for first time MGMT VPC Deployment

--- jenkins/scripts/stack_builder.sh
+++ jenkins/scripts/stack_builder.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 StackType=$(query "$params" CloudFormation.StackType)
 Project=$(query "$params" CloudFormation.Project)
+Peering=$(query "$params" CloudFormation.Peering)
 
 
 aws cloudformation create-stack \
-  --stack-name ${Project}-test \
+  --stack-name ${Project} \
   --template-url https://s3-eu-west-1.amazonaws.com/nordcloud-${bucket}/${folder}/vpc.template \
   --region eu-west-1 \
   --capabilities CAPABILITY_IAM \
   --parameters \
   ParameterKey=StackType,ParameterValue=${StackType} \
-  ParameterKey=Project,ParameterValue=${Project}
+  ParameterKey=Project,ParameterValue=${Project} \
+  ParameterKey=Peering,ParameterValue=${Peering}
