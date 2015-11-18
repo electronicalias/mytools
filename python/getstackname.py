a995eb3ed56aaaab51378d364046f2d88cd21213
electronicalias <electronicalias@gmail.com>
2015-11-18 20:48:48 +0000

    adding all requirements for the script to run without static values from within bash

--- python/getstackname.py
+++ python/getstackname.py
@@ -1,40 +1,27 @@
 import boto
 import boto.cloudformation
 import os
+import sys
 
-profile='default'
-region='eu-west-1'
-stackName='uat'
+profile=sys.argv[0]
+region=sys.argv[1]
+stackName=sys.argv[2]
 
 cf_conn = boto.cloudformation.connect_to_region(region, profile_name=profile)
 
 
 def getStackResource(stackName):
-#Function to get a resource ID created in a CF stack. Required Stack Name, and the Name of the Resouce it created.
-#Will Return ID of the resource specified or '1' if resource doesnt exist.
+# Function to get a Database Stack ID created in a CF stack. Requires the stackname for which you are trying to get the Database Stack from.
     try:
         stackResource=cf_conn.describe_stack_resources(stackName)
         for resource in stackResource:
             if 'Database' in resource.physical_resource_id:
                 stackid = resource.physical_resource_id
                 stackname = stackid.split("/")
-                print stackname[1]
-        return stackResource
+        return stackname[1]
     except Exception as error:
         print("Error when getting information about stack resource {} in stack {}. ****Stack Trace: {} ****".format(stackName,error))
         return (1)
 
-#Hear, we query the master stack for the ID of the database stack, and then \
-# within the same command query the database stack for the RDS Database ID.
-# databaseid=getStackResource(getStackResource(stackName,"DatabaseStack"),"RDSDatabase")
-getStackResource(stackName)
-
-# Use this to just get the name of the database stack...
-#getStackResource(stackName,"DatabaseStack")
-
-# print("DatabaseID is: {}".format(databaseid))
-# print("Database Stack name is: {}".format(dbstackname))
-
-#Write the databaseid to a file called database_id, so this can be read in later...
-# with open('database_id', 'w') as database_id:
-#    database_id.write(databaseid)
+# Here, we query the master stack for the ID of the database stack. We pass this value into the Jenkins job to carry to the next job.
+dbstack = getStackResource(stackName)
