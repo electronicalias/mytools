c450812d09eb29e8b29a3366e97cc38a16adaaa1
Administrator <Administrator@CRUGROUP.INTERNAL>
2015-11-19 04:04:51 +0000

    working!

--- python/test.py
+++ python/test.py
@@ -11,7 +11,7 @@ cursor = conn.cursor()
 cursor.execute("""SELECT TABLE_NAME FROM InteractiveTool.INFORMATION_SCHEMA.Tables """)
 
 for row in cursor:
-    filename = print ("%s" % (row))
+    filename = ("%s" % (row))
     dbout = subprocess.call("bcp InteractiveTool.dbo."+ filename +" out C:\\"+ filename +".txt -n -S WIN-AMNJ64HBK41\SQLEXPRESS -U test -P blah", shell=True)
     dbin = subprocess.call("bcp InteractiveTool.dbo."+ filename +" in C:\\"+ filename +".txt -n -S cru-poc-rds-db.cpqrib8hpigb.eu-west-1.rds.amazonaws.com -U crumasterdba -P 6*(HF3!um:+H", shell=True)
 
