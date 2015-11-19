e443e12226acec9fb1f3c82d9e0a62556d89d3c4
root <root@ip-172-31-35-77.eu-west-1.compute.internal>
2015-11-19 03:54:46 +0000

    adding print

--- python/test.py
+++ python/test.py
@@ -2,8 +2,6 @@ import pymssql
 import os
 import subprocess
 
-dir = subprocess.call("bcp InteractiveTool.dbo.commodity out C:\\blah.txt -n -S WIN-AMNJ64HBK41\SQLEXPRESS -U test -P blah", shell=True)
-dir = subprocess.call("bcp InteractiveTool.dbo.commodity in C:\\blah.txt -n -S cru-poc-rds-db.cpqrib8hpigb.eu-west-1.rds.amazonaws.com -U crumasterdba -P 6*(HF3!um:+H", shell=True)
 user = 'crumasterdba'
 password = '6*(HF3!um:+H'
 server = 'cru-poc-rds-db.cpqrib8hpigb.eu-west-1.rds.amazonaws.com'
@@ -13,8 +11,9 @@ cursor = conn.cursor()
 cursor.execute("""SELECT TABLE_NAME FROM InteractiveTool.INFORMATION_SCHEMA.Tables """)
 
 for row in cursor:
-    dbout = subprocess.call("bcp InteractiveTool.dbo."+ row +" out C:\\"+ row +".txt -n -S WIN-AMNJ64HBK41\SQLEXPRESS -U test -P blah", shell=True)
-    dbin = subprocess.call("bcp InteractiveTool.dbo."+ row +" in C:\\"+ row +".txt -n -S cru-poc-rds-db.cpqrib8hpigb.eu-west-1.rds.amazonaws.com -U crumasterdba -P 6*(HF3!um:+H", shell=True)
+    filename = print ("%s" % (row))
+    dbout = subprocess.call("bcp InteractiveTool.dbo."+ filename +" out C:\\"+ filename +".txt -n -S WIN-AMNJ64HBK41\SQLEXPRESS -U test -P blah", shell=True)
+    dbin = subprocess.call("bcp InteractiveTool.dbo."+ filename +" in C:\\"+ filename +".txt -n -S cru-poc-rds-db.cpqrib8hpigb.eu-west-1.rds.amazonaws.com -U crumasterdba -P 6*(HF3!um:+H", shell=True)
 
 conn.commit()
 conn.close()
