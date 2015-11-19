25a2177aacc36a8831d415dddc404e7f55ec0126
electronicalias <electronicalias@gmail.com>
2015-11-19 09:11:53 +0000

    removing any creds

--- python/test.py
+++ python/test.py
@@ -3,9 +3,12 @@ import os
 import subprocess
 import sys
 
-user = sys.argv[1]
-password = sys.arv[2]
-server = sys.argv[3]
+source.user = sys.argv[1]
+source.password = sys.arv[2]
+source.server = sys.argv[3]
+dest.user = sys.argv[4]
+dest.password = sys.arv[5]
+dest.server = sys.argv[6]
 
 conn = pymssql.connect(server, user, password, "InteractiveTool")
 cursor = conn.cursor()
@@ -13,8 +16,8 @@ cursor.execute("""SELECT TABLE_NAME FROM InteractiveTool.INFORMATION_SCHEMA.Tabl
 
 for row in cursor:
     filename = ("%s" % (row))
-    dbout = subprocess.call("bcp InteractiveTool.dbo."+ filename +" out C:\\"+ filename +".txt -n -S WIN-AMNJ64HBK41\SQLEXPRESS -U test -P blah", shell=True)
-    dbin = subprocess.call("bcp InteractiveTool.dbo."+ filename +" in C:\\"+ filename +".txt -n -S cru-poc-rds-db.cpqrib8hpigb.eu-west-1.rds.amazonaws.com -U crumasterdba -P 6*(HF3!um:+H", shell=True)
+    dbout = subprocess.call("bcp InteractiveTool.dbo."+ filename +" out C:\\"+ filename +".txt -n -S "+ source.server +" -U "+ source.user +" -P "+ source.password +"", shell=True)
+    dbin = subprocess.call("bcp InteractiveTool.dbo."+ filename +" in C:\\"+ filename +".txt -n -S "+ dest.server +" -U "+ dest.user +" -P "+ dest.password +"", shell=True)
 
 conn.commit()
 conn.close()
