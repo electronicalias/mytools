2b1c75a9d2518329abbc7157ee86685f4004a38e
root <root@ip-172-31-35-77.eu-west-1.compute.internal>
2015-11-19 03:50:58 +0000

    test 3

--- python/test.py
+++ python/test.py
@@ -13,7 +13,8 @@ cursor = conn.cursor()
 cursor.execute("""SELECT TABLE_NAME FROM InteractiveTool.INFORMATION_SCHEMA.Tables """)
 
 for row in cursor:
-    print ("%s" % (row))
+    dbout = subprocess.call("bcp InteractiveTool.dbo."+ row +" out C:\\"+ row +".txt -n -S WIN-AMNJ64HBK41\SQLEXPRESS -U test -P blah", shell=True)
+    dbin = subprocess.call("bcp InteractiveTool.dbo."+ row +" in C:\\"+ row +".txt -n -S cru-poc-rds-db.cpqrib8hpigb.eu-west-1.rds.amazonaws.com -U crumasterdba -P 6*(HF3!um:+H", shell=True)
 
 conn.commit()
 conn.close()
