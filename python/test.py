4e08639a833971834716ecc56eb878dd5d39a7ce
root <root@ip-172-31-35-77.eu-west-1.compute.internal>
2015-11-19 03:11:32 +0000

    added functionality to the scripts for SQL

--- python/test.py
+++ python/test.py
@@ -4,22 +4,12 @@ user = 'crumasterdba'
 password = '6*(HF3!um:+H'
 server = 'cru-poc-rds-db.cpqrib8hpigb.eu-west-1.rds.amazonaws.com'
 
-conn = pymssql.connect(server, user, password, "test")
+conn = pymssql.connect(server, user, password, "InteractiveTool")
 cursor = conn.cursor()
-cursor.execute("""
-IF OBJECT_ID('persons', 'U') IS NOT NULL
-    DROP TABLE persons
-CREATE TABLE persons (
-    id INT NOT NULL,
-    name VARCHAR(100),
-    salesrep VARCHAR(100),
-    PRIMARY KEY(id)
-)
-""")
-cursor.executemany(
-    "INSERT INTO persons VALUES (%d, %s, %s)",
-    [(1, 'John Smith', 'John Doe'),
-     (2, 'Jane Doe', 'Joe Dog'),
-     (3, 'Mike T.', 'Sarah H.')])
+cursor.execute("""SELECT TABLE_NAME FROM InteractiveTool.INFORMATION_SCHEMA.Tables """)
+
+for row in cursor:
+    print ("%s" % (row))
+
 conn.commit()
 conn.close()
