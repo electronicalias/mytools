51dcd300331ae6bd093cb00f715f9bf5444fc221
Philip Smith <philip.smith@nordcloud.com>
2015-11-30 23:52:15 +0000

    trying this

--- cloudtrail/sort_it_out.py
+++ cloudtrail/sort_it_out.py
@@ -1,1 +1,11 @@
 import boto.cloudformation
+import argparse
+
+arser = argparse.ArgumentParser(prog='Attributes Collection')
+parser.add_argument('--iamregion')
+parser.add_argument('--stackName')
+args = parser.parse_args()
+
+cf_conn = boto.cloudformation.connect_to_region(args.iamregion)
+
+print cf_conn.describe_stack_resources(args.stackName)
