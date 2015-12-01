90005773c28c4c1e84cd226b18ede155e487d8d2
Philip Smith <philip.smith@nordcloud.com>
2015-12-01 15:54:42 +0000

    set to update_trail

--- cloudtrail/sort_it_out.py
+++ cloudtrail/sort_it_out.py
@@ -109,7 +109,7 @@ def get_loggroup_arn(logArn):
 
 def configure_trail(name, sns_topic_name, cloud_watch_logs_log_group_arn, cloud_watch_logs_role_arn):
     try:
-        ct_conn.update_cloudtrail(
+        ct_conn.update_trail(
             name,
             sns_topic_name,
             cloud_watch_logs_log_group_arn,
