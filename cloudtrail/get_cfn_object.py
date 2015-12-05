bf0cd7660f896cd1f46bb440a9dd8d8540f69a1a
Philip Smith <philip.smith@nordcloud.com>
2015-12-05 00:15:11 +0000

    testing classes

--- cloudtrail/get_cfn_object.py
+++ cloudtrail/get_cfn_object.py
@@ -2,9 +2,20 @@ import boto.cloudformation
 
 cf_conn = boto.cloudformation.connect_to_region(region_name='eu-west-1')
 
+class stacks:
+
+	def __init__(self, name, region):
+		self.name = name
+		self.stacklist = []
+		self.regions = []
+
+	def add_stack(self, stack):
+		self.stacklist.append(stack)
+
+
 stacks = cf_conn.describe_stacks('cloudtrail-iams')
 if len(stacks) == 1:
     stack = stacks[0]
 
 for output in stack.outputs:
-    print('%s' % (output.value))
+    stacks.add_stack(output.value)
