293242e30a1dc3627c88394a107a8a036240b10f
electronicalias <electronicalias@gmail.com>
2016-01-26 14:50:36 +0000

    adding looping for every disk

--- powershell/custom_metric.ps1
+++ powershell/custom_metric.ps1
@@ -1,20 +1,24 @@
 param (
     [string]$region = $(throw "-region is required"),
-    [string]$disk = $(throw "-disk is required.")
+    [string]$device = $(throw "-device is required.")
  )
 
 Import-Module AWSPowerShell
-Set-DefaultAWSRegion -Region sa-east-1
+Set-DefaultAWSRegion -Region $region
 
-$disk = Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='C:'"
+$disk = Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='${device}:'"
 $PercentFree = [Math]::round((($disk.freespace/$disk.size) * 100))
 
 $InstanceId = invoke-restmethod -uri http://169.254.169.254/latest/meta-data/instance-id
 $dimensions = New-Object System.Collections.Generic.List``1[Amazon.CloudWatch.Model.Dimension]
-$dimension = New-Object -TypeName Amazon.CloudWatch.Model.Dimension
-$dimension.Name = "InstanceId"
-$dimension.Value = $InstanceId
-$dimensions.Add($dimension)
+$dimension1 = New-Object -TypeName Amazon.CloudWatch.Model.Dimension
+$dimension1.Name = "InstanceId"
+$dimension1.Value = $InstanceId
+$dimension2 = New-Object -TypeName Amazon.CloudWatch.Model.Dimension
+$dimension2.Name = "VolumeId"
+$dimension2.Value = ${device}
+$dimensions.Add($dimension1)
+$dimensions.Add($dimension2)
 
 $dat = New-Object Amazon.CloudWatch.Model.MetricDatum
 $dat.Timestamp = (Get-Date).ToUniversalTime() 
