c1b1e7a6a32f7df823570a757ea54992bb194136
electronicalias <electronicalias@gmail.com>
2016-01-26 11:49:11 +0000

    adding script to manage disk free

--- powershell/custom_metric.ps1
+++ powershell/custom_metric.ps1
@@ -1,9 +1,20 @@
-$disk = Get-WmiObject Win32_LogicalDisk -ComputerName remotecomputer -Filter "DeviceID='C:'" |
-Foreach-Object {$_.Size,$_.FreeSpace}
+Import-Module AWSPowerShell
+Set-DefaultAWSRegion -Region sa-east-1
+
+$disk = Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='C:'"
+$PercentFree = [Math]::round((($disk.freespace/$disk.size) * 100))
+
+$InstanceId = invoke-restmethod -uri http://169.254.169.254/latest/meta-data/instance-id
+$dimensions = New-Object System.Collections.Generic.List``1[Amazon.CloudWatch.Model.Dimension]
+$dimension = New-Object -TypeName Amazon.CloudWatch.Model.Dimension
+$dimension.Name = "InstanceId"
+$dimension.Value = $InstanceId
+$dimensions.Add($dimension)
 
 $dat = New-Object Amazon.CloudWatch.Model.MetricDatum
 $dat.Timestamp = (Get-Date).ToUniversalTime() 
-$dat.MetricName = "DiskFreeSpace"
-$dat.Unit = "Count"
-$dat.Value = $FreeSpace
+$dat.MetricName = "DiskFreePercent"
+$dat.Unit = "Percent"
+$dat.Value = $PercentFree
+$dat.Dimensions = $dimensions
 Write-CWMetricData -Namespace "Disk Metrics" -MetricData $dat
