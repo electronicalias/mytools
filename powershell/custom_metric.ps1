debdd5bc66ce20e0c9da15be3e79b124307dec42
Philip Smith <philip.smith@nordcloud.com>
2016-01-26 10:14:17 +0000

    disk metrics for cloud watch added

--- powershell/custom_metric.ps1
+++ powershell/custom_metric.ps1
@@ -1,2 +1,9 @@
 $disk = Get-WmiObject Win32_LogicalDisk -ComputerName remotecomputer -Filter "DeviceID='C:'" |
 Foreach-Object {$_.Size,$_.FreeSpace}
+
+$dat = New-Object Amazon.CloudWatch.Model.MetricDatum
+$dat.Timestamp = (Get-Date).ToUniversalTime() 
+$dat.MetricName = "DiskFreeSpace"
+$dat.Unit = "Count"
+$dat.Value = $FreeSpace
+Write-CWMetricData -Namespace "Disk Metrics" -MetricData $dat
