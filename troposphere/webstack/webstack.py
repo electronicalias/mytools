6c74fc63bdc25513a58391a2e7e027952b206e26
Philip Smith <electronicalias@gmail.com>
2016-01-29 08:20:10 +0000

    adding updates

--- troposphere/webstack/webstack.py
+++ troposphere/webstack/webstack.py
@@ -23,19 +23,51 @@ for i in cmd.az('production', 'public'):
     resources[str(i['SubnetId'])] = str(i['AvailabilityZone'])
 
 print resources
-len(resources)
+print(len(resources))
 
 count = 1
+def subnet(value):
+    string = t.add_parameter(Parameter(
+        "SubnetId" + str(count),
+        Description="Subnet Id of the network where nodes are deployed",
+        Type="String",
+        Default=value,
+        ))
+    return "SunbetId" + str(count) 
 
 t = Template()
 
-for key, value in resources.iteritems():
-    subnet_param = template.add_parameter(Parameter(
-        "SubnetId",
-        Description="Subnet Id of the network where nodes are deployed",
-        Type="String",
-        Default=args.subnet_id,
-        ))
-    return subnet_param
+params = {}
+for num in range(len(resources) + 1):
+    params[str([num])] = ''
 
+for key, value in resources.iteritems():
+    subnet(value)
+    count += 1
+
+subnets = []
+azs = []
+
+for key, value in resources.iteritems():
+    subnets.append(value)
+
+for key, value in resources.iteritems():
+    azs.append(key)
+
+autoscaling_group = t.add_resource(asc.AutoScalingGroup(
+    "AutoscalingGroup",
+    DesiredCapacity=0,
+    Tags=[
+        asc.Tag("Environment", "HPC", True),
+        asc.Tag("RequestedBy", "psmith", True),
+        asc.Tag("Job", "hpc-spot", True),
+    ],
+    LaunchConfigurationName="somelc",
+    MinSize=0,
+    MaxSize=0,
+    VPCZoneIdentifier=[ subnets ],
+    AvailabilityZones=[ azs ],
+    HealthCheckType="EC2",
+))
 print(t.to_json())
+print params
