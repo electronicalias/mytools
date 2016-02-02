f7dcf151d8a14dddaa1708aa6b6502b1beae951b
Philip Smith <electronicalias@gmail.com>
2016-02-02 13:19:18 +0000

    adding the creds/role helper

---
+++ powershell/creds-helper.ps1
@@ -0,0 +1,20 @@
+﻿<#
+Designed as a credential helper which can be used/called as a program that will output the results
+and allow a script downflow to use the credentials
+#>
+
+param (
+    [string]$region = $(throw "-region is required"),
+    [string]$accKey = $(throw "-accKey is required"),
+    [string]$secKey = $(throw "-secKey is required"),
+    [string]$roleArn = $(throw "-roleArn is required"),
+    [string]$roleName = $(throw "-roleName is required")
+ )
+
+Import-Module AWSPowerShell
+Set-AWSCredentials -AccessKey $accKey -SecretKey $secKey -StoreAs tempProfile
+Initialize-AWSDefaults -ProfileName tempProfile -Region $region
+
+$Creds = (Use-STSRole -RoleArn $roleArn -RoleSessionName $roleName).Credentials
+
+Clear-AWSCredentials -StoredCredentials tempProfile
