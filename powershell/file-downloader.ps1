3794d497943c27363864efba65a26238cea92e5b
electronicalias <electronicalias@gmail.com>
2016-03-03 13:58:51 +0000

    added explanation of the file

--- powershell/file-downloader.ps1
+++ powershell/file-downloader.ps1
@@ -1,3 +1,12 @@
+<#
+This script does the following:
+1. collects the command line parameters
+2. gets a list of files in the specified bucket/folder
+3. for each object, copy it to the destFolder
+  
+  downloader.ps1 -bucketName SOMEBUCKET -destFolder "D:\DTU" -customerFolder "CustomerName(must be same as Username)"
+#>
+
 param (
     [string]$destFolder = $(throw "-destFolder is required!"),
     [string]$bucketName = $(throw "-bucketName is required!"),
