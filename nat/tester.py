846447a0d5eab83ddf6bb9e18f7c7391f77d65b2
electronicalias <electronicalias@gmail.com>
2016-03-15 12:12:35 +0000

    adding new functionality to NAT script

--- nat/tester.py
+++ nat/tester.py
@@ -4,9 +4,16 @@ import boto3
 import urllib
 import urllib2
 
-testurls = ["http://www.google.co.uk", "http://www.pool.ntp.org", "http://www.github.com"]
+url1 = 'http://www.google.co.uk'
+url2 = 'http://www.pool.ntp.org'
+url3 = 'http://www.github.com'
 
 client = boto3.client('ec2','ap-southeast-1')
+ec2 = boto3.resource('ec2','ap-southeast-1')
+
+def check_url(url):
+    request = urllib2.urlopen(url)
+    return request.code
 
 def get_private_route_tables():
     PrivateRouteTables = client.describe_route_tables(
@@ -27,15 +34,15 @@ def get_private_route_tables():
     return PrivateRouteTables['RouteTables']
 
 for RouteTable in get_private_route_tables():
-    print RouteTable['RouteTableId']
-    ec2 = boto3.resource('ec2','ap-southeast-1')
     route_table = ec2.RouteTable(RouteTable['RouteTableId'])
-    for route in route_table:
-        print(route_table.routes['DestinationCidrBlock'])
-
-for url in testurls:
-    request = urllib2.urlopen(url)
-    print request.code
+    for route in route_table.routes:
+        default = 'NoValue'
+        if '0.0.0.0' in (route.get('DestinationCidrBlock', default)):
+            if 'BlackHole' in route.get('State'):
+                print("This sucks!")
+            else:
+                print("We are good to go")
+                print(route.get('InstanceId'))
 
 InstanceId = urllib2.urlopen('http://169.254.169.254/latest/meta-data/instance-id').read()
 LocalIp = urllib2.urlopen('http://169.254.169.254/latest/meta-data/local-ipv4').read()
@@ -44,3 +51,6 @@ AvailabilityZone = urllib2.urlopen('http://169.254.169.254/latest/meta-data/plac
 print InstanceId
 print LocalIp
 print AvailabilityZone
+
+if (check_url(url1) == 200) or (check_url(url2) == 200) or (check_url(url3) == 200):
+    print("Connectivity is OK")
