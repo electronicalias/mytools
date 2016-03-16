ba870eef37983e63dc333864bbfbc6c2aa895c26
Philip Smith <philip.smith@nordcloud.com>
2016-03-16 15:23:27 +0000

    fix syntax error

--- nat/nat.py
+++ nat/nat.py
@@ -46,7 +46,7 @@ print LocalState
 PeerState = state_check(PeerIp)
 print PeerState
 
-RouteTables = aws.get_rt_tables(arg.vpc_id,'private'):
+RouteTables = aws.get_rt_tables(arg.vpc_id,'private')
 	print aws.get_table_id(RouteTables)
 
 # shell.cmd(str('/usr/bin/aws ec2 describe-instances --region ' + arg.region_name))
