2e20a1661a9dd8c7a60531afc61c8053868ae79d
Philip Smith <philip.smith@nordcloud.com>
2016-03-16 10:18:59 +0000

    changing to be a package/class

---
+++ nat/cmd.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import boto3
+
+class aws:
+	'''
+	Usage: can be imported as a module for the following functions:
+	Attach EIP, requires Instance Id
+	Set Source/Dest Check, requires Instance Id
+	'''
+    
+    def __init__(self,region):
+        self.region = region
+        self.ec2_client = boto3.client('ec2',region)
+        self.ec2_resource = boto3.resource('ec2',region)
+
+    def associate_eip(self,InstanceId,AllocationId):
+        ''' Associate the given InstanceId to the AllocationId '''
+        response = self.ec2_client.associate_address(
+            InstanceId=InstanceId,
+            AllocationId=AllocationId,
+            AllowReassociation=True
+        )
+    	return response
---
+++ nat/nat.py
@@ -0,0 +1,22 @@
+#!/usr/env/bin python
+''' Import Modules '''
+import urllib2
+import argparse
+import cmd
+
+InstanceId = urllib2.urlopen('http://169.254.169.254/latest/meta-data/instance-id').read()
+
+''' Setup the Command Line to accept the variables required '''
+parser = argparse.ArgumentParser(
+    prog='Nat Updater',
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description='''
+    This program is used to re-attach the EIP and set the source-dest-check to false so this instance can take over
+    the NAT function.
+    ''')
+parser.add_argument('-a','--allocation_id', required=True)
+parser.add_argument('-r','--region_name', required=True)
+arg = parser.parse_args()
+
+aws = cmd.aws(region_name)
+aws.associate_eip(InstanceId,arg.allocation_id)
