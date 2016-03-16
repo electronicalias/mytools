18196f5d22f5108649868af7f0efaccd5d0bfd16
Philip Smith <philip.smith@nordcloud.com>
2016-03-16 12:58:10 +0000

    remove cut from ends with

--- nat/nat.py
+++ nat/nat.py
@@ -6,9 +6,9 @@ import cmd
 
 InstanceId = urllib2.urlopen('http://169.254.169.254/latest/meta-data/instance-id').read()
 AvailabilityZone = urllib2.urlopen('http://169.254.169.254/latest/meta-data/placement/availability-zone').read()
-if AvailabilityZone[:1].endswith('a'):
+if AvailabilityZone.endswith('a'):
     PeerAz = str(AvailabilityZone[:-1] + 'b')
-elif AvailabilityZone[:1].endswith('b'):
+elif AvailabilityZone.endswith('b'):
     PeerAz = str(AvailabilityZone[:-1] + 'a')
 
 print(PeerAz)
