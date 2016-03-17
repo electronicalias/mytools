85ff074ed3a7e5ab017bb6792698fe30eea21bb4
Philip Smith <philip.smith@nordcloud.com>
2016-03-17 02:11:27 +0000

    set state

--- nat/nat.py
+++ nat/nat.py
@@ -58,8 +58,9 @@ for table in aws.get_rt_tables(arg.vpc_id,'private'):
     		break
         if '0.0.0.0' in (route.get('DestinationCidrBlock', default)):
         	if 'standby' in aws.get_tag(InstanceId) and 'BlackHole' in (route.get('Status')):
-        	    aws.associate_eip(InstanceId,arg.allocation_id)
+                aws.associate_eip(InstanceId,arg.allocation_id)
                 shell.cmd(str('/usr/bin/aws ec2 replace-route --route-table-id ' + table_id.route_table_id + ' --destination-cidr-block 0.0.0.0/0 --instance-id ' + InstanceId + ' --region ' + arg.region_name))
+                aws.set_tag(InstanceId,'active')
                 syslog.syslog(str('Moved NAT due to BlackHole in the route, to: ' + InstanceId))
                     
             DestBlock = route.get('DestinationCidrBlock')
