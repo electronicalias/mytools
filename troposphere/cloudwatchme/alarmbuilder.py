ad04a47bea0fc747af282db2e19192a4daa16c0a
electronicalias <electronicalias@gmail.com>
2016-03-29 22:54:09 +0000

    added functions and logic for working out what the metric will be

--- troposphere/cloudwatchme/alarmbuilder.py
+++ troposphere/cloudwatchme/alarmbuilder.py
@@ -21,60 +21,75 @@ parser.add_argument('-rgn','--region_name', required=True)
 parser.add_argument('-tpn','--topic_name', required=True)
 arg = parser.parse_args()
 
+ec2 = boto3.client('elb',arg.region_name)
+sns = boto3.client('sns',arg.region_name)
+cfn = boto3.client('cloudformation',arg.region_name)
+elbs = ec2.describe_load_balancers()
+
 def topic_arn():
     arn = sns.list_topics()
     for item in arn['Topics']:
         if arg.topic_name in item['TopicArn']:
             return item['TopicArn']
 
-def param(name):
+def param(name, param_type):
+    res_name = str(name).replace('-','')
     name = t.add_parameter(Parameter(
-        name + 'Param',
-        Description="Auto Generated Param",
+        res_name + 'Param',
+        Description=str('Auto Generated Param for: ' + param_type + ' : ' + name),
         Type="String",
         Default=name,
     ))
 
-def elb(name,topic):
+def elb(name, metric_type):
+    if 'Latency' in metric_type:
+        stat_type = 'Average'
+    res_name = str(name).replace('-','')
     name = t.add_resource(
         Alarm(
-            name,
+            res_name,
             AlarmDescription="SurgeQueueLength Alarm if the queue is over 100 max per minute.",
             Namespace="AWS/ELB",
-            MetricName="SurgeQueueLength",
+            MetricName=metric_type,
             Dimensions=[
                 MetricDimension(
                     Name="LoadBalancerName",
-                    Value=Ref(str(name + 'Param'))
+                    Value=Ref(str(res_name) + 'Param')
                 ),
             ],
-            Statistic="Maximum",
+            Statistic=stat_type,
             Period="60",
-            EvaluationPeriods="3",
-            Threshold="100",
+            EvaluationPeriods="5",
+            Threshold="0.6",
             ComparisonOperator="GreaterThanOrEqualToThreshold",
-            AlarmActions=[topic],
+            AlarmActions=[Ref(sns_topic_name)],
         )
     )
+
+topic = topic_arn()
 
 t = Template()
 
 t.add_description("""\
 This script is automatically deployed by Jenkins, the script first lists all of the ELBs in a region and then installs the alarms specified for each...""")
 
-topic = topic_arn()
+sns_topic_name = t.add_parameter(Parameter(
+    "SnsTopicArn",
+    Description=str("Auto Generated Paramemerter for an SNS Topic"),
+    Type="String",
+    Default=topic,
+))
 
 for i in elbs['LoadBalancerDescriptions']:
-    param(str(i['LoadBalancerName']).replace('-',''))
-    elb(str(i['LoadBalancerName']).replace('-',''),topic)
+    param(str(i['LoadBalancerName']), 'ELB')
+    elb(str(i['LoadBalancerName']), 'Latency')
 
 # print(t.to_json())
 
-cfn = boto3.client('cloudformation', 'us-east-1')
 cfn_body = t.to_json()
 
 response = cfn.create_stack(
-    StackName='CloudWatch-Test',
+    StackName='CloudWatch-Alarms',
     TemplateBody=cfn_body,
     Capabilities=[
         'CAPABILITY_IAM',
