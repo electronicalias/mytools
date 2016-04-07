c5323972af15da78111e7b71f71a5a3195c51196
electronicalias <electronicalias@gmail.com>
2016-04-07 07:32:18 +0000

    changing tag from Project to Area

--- python/get_subnets.py
+++ python/get_subnets.py
@@ -27,7 +27,7 @@ def get_subnet():
                 ]
             },
             {
-                'Name': 'tag:Project',
+                'Name': 'tag:Area',
                 'Values': [
                     arg.area_name,
                 ]
